d84142357472889ec866855217cd5748d64b5ce9
Jake Glassman & Michael Bunker <student@epicodus.com>
2015-10-14 09:54:41 -0700

    delete functionality for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,4 +8,28 @@ class ReviewsController < ApplicationController
     # @user = User.find(params[:id])
   end
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to reviews_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:title, :content, :user_id)
+  end
+
 end

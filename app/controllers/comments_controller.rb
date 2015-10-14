703bcbccc4eadbd251c50ceebfdb185278f92e89
Jake Glassman & Michael Bunker <student@epicodus.com>
2015-10-14 15:01:48 -0700

    delete functionality for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    review = Review.find(params[:review_id])
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to review_path(review)
+  end
+
 private
   def comment_params
     params.require(:comment).permit(:content)
